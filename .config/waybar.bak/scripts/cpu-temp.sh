2766c8970bc8fb5162afa36e113164c1cb6a91a0
Twilight4 <electrolight071@gmail.com>
2023-12-07 20:13:30 +0100

    update

--- .config/waybar.bak/scripts/cpu-temp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Since string manipulations in bash can't be nested, temporary variables are necessary.
-a="$(acpi -t)";a="${a#*, }";printf '%s°C\n' "${a% d*}"
--- .config/waybar.bak/scripts/vpn-check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $(ip a s | grep tun0 | grep inet | awk '{print $2;}' | cut -d . -f2 | tr -d '\n' | wc -c) -eq 2 ]; then
-  echo "ON"
-elif [ $(ip a s | grep tun0 | grep inet | awk '{print $2;}' | cut -d . -f2 | tr -d '\n' | wc -c) -eq 0 ]; then
-  echo "OFF"
-fi
